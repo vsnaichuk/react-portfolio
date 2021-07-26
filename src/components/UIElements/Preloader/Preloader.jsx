e476805327b0597dece9fe4bc142af8dc02f870a
VolodumurSN <v.snaichuk@gmail.com>
2021-07-26 08:49:13 +0300

    Add React Suspence and lazy. Implement preloader

---
+++ src/components/UIElements/Preloader/Preloader.jsx
@@ -0,0 +1,9 @@
+import s from './Preloader.module.scss';
+
+const Preloader = () => {
+  return (
+    <div className={s.preloader} />
+  );
+};
+
+export default Preloader;
--- src/routes/BaseRoutes.jsx
+++ src/routes/BaseRoutes.jsx
@@ -1,13 +1,15 @@
+import { lazy, Suspense} from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Switch,
 } from 'react-router-dom';
+import Preloader from '../components/UIElements/Preloader/Preloader';
 //scenes
-import Home from '../scenes/Home/Home';
-import About from '../scenes/About/About';
-import Projects from '../scenes/Projects/Projects';
-import Resume from '../scenes/Resume/Resume';
+const Home = lazy(() => import('../scenes/Home/Home'));
+const About = lazy(() => import( '../scenes/About/About'));
+const Projects = lazy(() => import( '../scenes/Projects/Projects'));
+const Resume = lazy(() => import( '../scenes/Resume/Resume'));
 
 export const routes = {
   HOME: '/',
@@ -18,14 +20,16 @@ export const routes = {
 
 const BaseRoutes = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path={routes.HOME} exact component={Home} />
-        <Route path={routes.ABOUT} component={About} />
-        <Route path={routes.PROJECTS} component={Projects} />
-        <Route path={routes.RESUME} component={Resume} />
-      </Switch>
-    </Router>
+    <Suspense fallback={<Preloader />}>
+      <Router>
+        <Switch>
+          <Route path={routes.HOME} exact component={Home} />
+          <Route path={routes.ABOUT} component={About} />
+          <Route path={routes.PROJECTS} component={Projects} />
+          <Route path={routes.RESUME} component={Resume} />
+        </Switch>
+      </Router>
+    </Suspense>
   );
 };
 
