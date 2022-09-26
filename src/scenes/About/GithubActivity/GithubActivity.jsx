ff40a2d9d6f80bbd609cb2d18c9030394ed95471
Snaichuk Volodymyr <s.volodymyr@apiko.com>
2022-09-26 18:04:22 +0300

    Update GitHubCalendar

--- src/scenes/About/GithubActivity/GithubActivity.jsx
+++ src/scenes/About/GithubActivity/GithubActivity.jsx
@@ -5,11 +5,11 @@ const GithubActivity = () => {
   const colorTheme = {
     background: 'transparent',
     text: '#ffffff',
-    grade4: '#8400b8',
-    grade3: '#b22ff4',
-    grade2: '#b265f6',
-    grade1: '#c084f5',
-    grade0: '#ecd9fc',
+    level4: '#8400b8',
+    level3: '#b22ff4',
+    level2: '#b265f6',
+    level1: '#c084f5',
+    level0: '#ecd9fc',
   };
   return (
     <div className={s.container}>
