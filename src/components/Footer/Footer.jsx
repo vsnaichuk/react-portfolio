a805d581bb2275d15af4d02438b6eb80436ac010
milliorn <scottmilliorn@gmail.com>
2023-04-16 22:39:09 -0700

    Heading levels should only increase by one

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
     <div className={s.footer}>
       <div className={s.container}>
         <div className={s.copyright}>
-          <h3>Made with ❤️ by Snaichuk Volodymyr</h3>
+          <h1>Made with ❤️ by Snaichuk Volodymyr</h1>
         </div>
 
         <div className={s.body}>
